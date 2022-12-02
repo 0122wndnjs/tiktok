44e95c002b403c5451530034fae2baec17e9afde
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-12-02 16:28:11 +0900

    using zustand and connect oauth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,31 +3,40 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 // import { GoogleLogin, GoogleLogout } from "react-google-login";
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
+import { createOrGetUser } from "../utils";
+import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
-  const user = false;
+  // const user = false;
+  const { userProfile, addUser } = useAuthStore();
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
         <div className="w-[100px] md:w-[130px]">
-          <Image className="cursor-pointer" src={Logo} alt='TikTik' layout="responsive" />
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="TikTik"
+            layout="responsive"
+          />
         </div>
       </Link>
 
+      <div>SEARCH</div>
+
       <div>
-        SEARCH
-      </div>
-      
-      <div>
-        {user ? (
-          <div>Logged In</div>
+        {userProfile ? (
+          <div>{userProfile.userName}</div>
         ) : (
-          <GoogleLogin onSuccess={(response) => console.log(response)} onError={() => console.log("Error")} />
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => console.log("Error")}
+          />
         )}
       </div>
     </div>
