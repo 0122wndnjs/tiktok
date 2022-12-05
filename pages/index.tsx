d3b02d669ff9e7c4682e9db0bc3f4940444007cf
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-12-05 12:20:54 +0900

    finishing uploading video but having error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,32 @@
-import axios from 'axios'
-import NoResults from '../components/NoResults';
-import VideoCard from '../components/VideoCard';
-import { Video } from '../types';
+import axios from "axios";
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+import { Video } from "../types";
 
-interface IProps { 
-  videos: Video[]
+interface IProps {
+  videos: Video[];
 }
 
 const Home = ({ videos }: IProps) => {
-  console.log(videos)
   return (
-    <div className='flex flex-col gap-10 videos h-full'>
+    <div className="flex flex-col gap-10 videos h-full">
       {videos.length ? (
-        videos.map((video: Video) => (
-          <VideoCard post={video} key={video._id} />
-        ))
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
       ) : (
-        <NoResults text={'No Videos'} />
+        <NoResults text={"No Videos"} />
       )}
     </div>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async () => {
   const { data } = await axios.get(`http://localhost:3000/api/post`);
 
   return {
     props: {
-      videos: data
-    }
-  }
-}
+      videos: data,
+    },
+  };
+};
 
-export default Home
+export default Home;
