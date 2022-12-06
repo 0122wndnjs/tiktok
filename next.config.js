6da6be8e013dd8d7cf9be60f2b29c6237e306e42
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-12-06 14:11:30 +0900

    adding swcMinify

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
+  swcMinify: false,
   reactStrictMode: true,
   images: {
     domains: ["yt3.ggpht.com", "lh3.googleusercontent.com"],
